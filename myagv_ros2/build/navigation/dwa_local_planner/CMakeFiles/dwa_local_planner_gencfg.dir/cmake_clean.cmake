9765d34542994416fb119bf2d93bce2119293702
dbs8998 <dbstjq34a2@gmail.com>
2025-10-29 16:26:11 +0900

    김윤섭 - git 파일 업로드

---
+++ myagv_ros2/build/navigation/dwa_local_planner/CMakeFiles/dwa_local_planner_gencfg.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "/home/geon/myagv_ros/devel/include/dwa_local_planner/DWAPlannerConfig.h"
+  "/home/geon/myagv_ros/devel/lib/python3/dist-packages/dwa_local_planner/cfg/DWAPlannerConfig.py"
+  "/home/geon/myagv_ros/devel/share/dwa_local_planner/docs/DWAPlannerConfig-usage.dox"
+  "/home/geon/myagv_ros/devel/share/dwa_local_planner/docs/DWAPlannerConfig.dox"
+  "/home/geon/myagv_ros/devel/share/dwa_local_planner/docs/DWAPlannerConfig.wikidoc"
+  "CMakeFiles/dwa_local_planner_gencfg"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/dwa_local_planner_gencfg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ myagv_ros2/devel/share/navfn/cmake/navfn-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(navfn_MESSAGE_FILES "")
+set(navfn_SERVICE_FILES "/home/geon/myagv_ros/src/navigation/navfn/srv/MakeNavPlan.srv;/home/geon/myagv_ros/src/navigation/navfn/srv/SetCostmap.srv")
