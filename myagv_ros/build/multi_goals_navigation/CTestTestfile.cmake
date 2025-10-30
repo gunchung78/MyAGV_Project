808dea4b6b669718aa46b49077e5d59d99667d16
김윤섭 <dbstjq34a2@gmail.com>
2025-10-30 12:24:46 +0900

    gitgnore 수정3

--- myagv_ros/build/multi_goals_navigation/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/geon/MyAGV_Project/myagv_ros/src/multi_goals_navigation
-# Build directory: /home/geon/MyAGV_Project/myagv_ros/build/multi_goals_navigation
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
